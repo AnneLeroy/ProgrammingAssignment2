067e3e4286f4731ee992ee76143fc280ccc7697e
Anne et Jacques Leroy <Anne et Jacques Leroy>
2016-05-08 10:56:16 +0200

    V 1.0

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,78 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
-}
+## Anne Leroy - May 8th 2016
+##
+#  Inversing a huge matrix can be time consuming
+#  It's interesting to compute this matrix only once
+#  and retrieve it's value from cache 
 
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+# 
+######  Please try (for example) : 
+#
+# matrice <- makeCacheMatrix(matrix(c(1, 0, 0,1), c(2, 2)))
+#
+# cacheSolve(matrice)
+#
+## You get : 
+##             [,1] [,2]
+##       [1,]    1    0
+##       [2,]    0    1
+#
+# cacheSolve(matrice)
+#
+## You get :
+##  Retrieve from cached data
+##             [,1] [,2]
+##       [1,]    1    0
+##       [2,]    0    1
+
+
+  
+
+
+
+ # This function creates the matrice and initialize cache to NULL
+ 
+ makeCacheMatrix <- function(x = matrix()) {
+         cache <- NULL
+         set <- function(y) {
+                 x <<- y
+                 cache <<- NULL
+         }
+         get <- function() x
+         setinverse <- function(inv) cache <<- inv
+         getinverse <- function() cache
+         list(
+                 set = set,
+                 get = get,
+                 setinverse = setinverse,
+                 getinverse = getinverse
+         )
+ }
+ 
+ 
+
+ 
+# 
+# This function inverse the matrix or retrieve the data from cache
+# 
+ cacheSolve <- function(x, ...) {
+         cache <- x$getinverse()
+         if(!is.null(cache)) {
+                 message("Retrieve from cached data")
+                 return(cache)
+         }
+         matrice <- x$get()
+         cache <- solve(matrice, ...)
+         x$setinverse(cache)
+         cache
+ }
+ 
+ 
+ # matrice <- makeCacheMatrix(matrix(c(1, 0, 0,1), c(2, 2)))
+ # cacheSolve(matrice)
+ # cacheSolve(matrice)
+
+ 
+ 
+ 
